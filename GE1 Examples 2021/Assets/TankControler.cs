1a4bba3656f25b316a2608f9bb287c6283f89494
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2021-09-25 12:48:12 +0100

    fixed camera to the tank so it trails

--- GE1 Examples 2021/Assets/TankControler.cs
+++ GE1 Examples 2021/Assets/TankControler.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class TankControler : MonoBehaviour
 {
-    public float speed = 5;
-    public float rotSpeed = 30;
+    public float speed = 6;
+    public float rotSpeed = 60;
     // Start is called before the first frame update
     void Start()
     {
