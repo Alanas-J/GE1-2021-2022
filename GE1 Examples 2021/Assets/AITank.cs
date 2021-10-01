48b1e92863fcdc854beaa9b9ecc2a0ca567168db
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2021-10-01 20:31:04 +0100

    Gizmos done, saving progress pre breakage

--- GE1 Examples 2021/Assets/AITank.cs
+++ GE1 Examples 2021/Assets/AITank.cs
@@ -10,18 +10,38 @@ public class AITank : MonoBehaviour {
     public int current = 0;
     List<Vector3> waypoints = new List<Vector3>();
     public float speed = 10;
+    public float rotSpeed = 30;
     public Transform player;    
 
     public void OnDrawGizmos()
     {
-        if (!Application.isPlaying)
-        {
+        if (!Application.isPlaying){
             // Task 1
             // Put code here to draw the gizmos
             // Use sin and cos to calculate the positions of the waypoints 
             // You can draw gizmos using
             // Gizmos.color = Color.green;
             // Gizmos.DrawWireSphere(pos, 1);
+
+            Vector3 gizmoPos;
+            Gizmos.color = Color.green;
+
+            // rotation translation calculator for gismos;
+            float theta = (Mathf.PI * 2.0f / numWaypoints); 
+            for(int i = 0; i < numWaypoints; i++){
+
+                // Translation math
+                gizmoPos =  new Vector3(Mathf.Cos(i*theta)*10,0,Mathf.Sin(i*theta)*10);
+                gizmoPos = gizmoPos + this.transform.position;
+                
+                // Adding the gizmos
+                Gizmos.color = Color.green; 
+                Gizmos.DrawWireSphere(gizmoPos,2);
+
+                
+            }
+            
+
         }
     }
 
@@ -30,6 +50,20 @@ public class AITank : MonoBehaviour {
         // Task 2
         // Put code here to calculate the waypoints in a loop and 
         // Add them to the waypoints List
+        Vector3 gizmoPos;
+        Gizmos.color = Color.green;
+
+        // rotation translation calculator for gismos;
+        float theta = (Mathf.PI * 2.0f / numWaypoints); 
+        for(int i = 0; i < numWaypoints; i++){
+
+            // Translation math
+            gizmoPos =  new Vector3(Mathf.Cos(i*theta)*10,0,Mathf.Sin(i*theta)*10);
+            gizmoPos = gizmoPos + this.transform.position;
+
+            // Adding position to list
+            waypoints.Add(gizmoPos);
+        }
     }
 
     // Update is called once per frame
@@ -37,7 +71,19 @@ public class AITank : MonoBehaviour {
         // Task 3
         // Put code here to move the tank towards the next waypoint
         // When the tank reaches a waypoint you should advance to the next one
+        
+        //localise position of gizmo
+        
+        //1. translate calculation to origin.
+        Vector3 translatedPoint = waypoint[current] - this.transform.position;
 
+        float angleToGizmo = translatedPoint.y
+        
+
+
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime, 0);
+        
 
         // Task 4
         // Put code here to check if the player is in front of or behine the tank
